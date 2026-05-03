79f7a82b99e4dd745ab02f4b9adf997a0e4e3a6b
Sora Tonami <ms0503@outlook.com>
2026-05-03 13:58:32 +0900

    refactor: replace `if let Err(_) = x {...}; let x = x.unwrap()` to `let Ok(x) = ... else {...};`

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,13 @@ async fn main() {
         let emojis = &args[1..];
         emojis.to_owned()
     };
-    let mut pool: Vec<JoinHandle<()>> = vec![];
+    let mut pool: Vec<JoinHandle<_>> = vec![];
     for emoji in emojis {
         pool.push(tokio::spawn(async move {
-            let emoji = emoji.clone();
-            download_emoji(&emoji).await
+            match download_emoji(&emoji).await {
+                Err(err) => eprintln!(":{}: : {}", emoji, err),
+                Ok(_) => println!(":{}: : Success", emoji)
+            }
         }));
     }
     for handle in pool {
@@ -60,31 +62,23 @@ async fn get_emoji_data(name: &str) -> Result<EmojiData, Box<dyn std::error::Err
     Ok(res)
 }
 
-async fn download_emoji(name: &str) {
-    let emoji = get_emoji_data(name).await;
-    if let Err(_) = emoji {
-        eprintln!(":{}: : failed to get metadata", name);
-        return;
-    }
-    let emoji = emoji.unwrap();
-    let data = reqwest::get(emoji.icon.url).await;
-    if let Err(_) = data {
-        eprintln!(":{}: : failed to get emoji", name);
-        return;
-    }
-    let data = data.unwrap().bytes().await;
-    if let Err(_) = data {
-        eprintln!(":{}: : failed to get emoji", name);
-        return;
-    }
-    let data = data.unwrap();
+async fn download_emoji(name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let Ok(emoji) = get_emoji_data(name).await else {
+        return Err("Failed to get metadata".into());
+    };
+    let Ok(data) = reqwest::get(emoji.icon.url).await else {
+        return Err("Failed to get emoji".into());
+    };
+    let Ok(data) = data.bytes().await else {
+        return Err("Failed to get emoji".into());
+    };
     match fs::write(
         format!("{}.{}", name, get_suffix(&emoji.icon.media_type)),
         data
     )
     .await
     {
-        Err(_) => eprintln!(":{}: : failed to write image file", name),
-        Ok(_) => println!(":{}: : success!", name)
+        Err(_) => Err("Failed to write image file".into()),
+        Ok(_) => Ok(())
     }
 }
