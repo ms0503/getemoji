5bb24c2e76588d3f00e4fa0ab4081abba1cf103a
Sora Tonami <ms0503@outlook.com>
2026-05-03 16:05:53 +0900

    feature: add validation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use lazy_regex::Lazy;
+use lazy_regex::Regex;
+use lazy_regex::lazy_regex;
+use reqwest::Url;
 use serde::Deserialize;
 use serde::Serialize;
 use std::env;
+use std::error::Error;
 use std::process;
 use tokio::fs;
 use tokio::task::JoinHandle;
+
+const MISSKEY_DOWNLOAD_HOST: &str = "media.misskeyusercontent.com";
+const MISSKEY_HOST: &str = "misskey.io";
+
+static MISSKEY_DOWNLOAD_EMOJI_PATH_PATTERN: Lazy<Regex> = lazy_regex!(
+    r#"^/misskey/webpublic-[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89a-d][0-9a-f]{3}-[0-9a-f]{12}\.[\w.\-_~]+$"#
+);
+static MISSKEY_GET_EMOJI_PATH_PATTERN: Lazy<Regex> = lazy_regex!(r#"^/emojis/[0-9_a-z]{2,}$"#);
 
 #[tokio::main]
 async fn main() {
@@ -54,37 +67,16 @@ fn get_suffix(mime_type: &str) -> &str {
     }
 }
 
-async fn get_emoji_data(name: &str) -> Result<EmojiData, Box<dyn std::error::Error>> {
-    let res = reqwest::get(format!("https://misskey.io/emojis/{}", name))
-        .await?
-        .json::<EmojiData>()
-        .await?;
+async fn get_emoji_data(name: &str) -> Result<EmojiData, Box<dyn Error>> {
+    let url = validate_emoji_url(name)?;
+    let res = reqwest::get(url).await?.json::<EmojiData>().await?;
     Ok(res)
 }
 
-fn is_allowed_icon_url(url: &reqwest::Url) -> bool {
-    if url.scheme() != "https" {
-        return false;
-    }
-    matches!(url.host_str(), Some("misskey.io"))
-}
-
-async fn download_emoji(name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let Ok(emoji) = get_emoji_data(name).await else {
-        return Err("Failed to get metadata".into());
-    };
-    let Ok(icon_url) = reqwest::Url::parse(&emoji.icon.url) else {
-        return Err("Invalid emoji URL".into());
-    };
-    if !is_allowed_icon_url(&icon_url) {
-        return Err("Untrusted emoji URL".into());
-    }
-    let Ok(data) = reqwest::get(icon_url).await else {
-        return Err("Failed to get emoji".into());
-    };
-    let Ok(data) = data.bytes().await else {
-        return Err("Failed to get emoji".into());
-    };
+async fn download_emoji(name: &str) -> Result<(), Box<dyn Error>> {
+    let emoji = get_emoji_data(name).await?;
+    let url = validate_download_url(&emoji.icon.url)?;
+    let data = reqwest::get(url).await?.bytes().await?;
     match fs::write(
         format!("{}.{}", name, get_suffix(&emoji.icon.media_type)),
         data
@@ -93,5 +85,41 @@ async fn download_emoji(name: &str) -> Result<(), Box<dyn std::error::Error>> {
     {
         Err(_) => Err("Failed to write image file".into()),
         Ok(_) => Ok(())
+    }
+}
+
+fn validate_emoji_url(name: &str) -> Result<Url, Box<dyn Error>> {
+    let url = Url::parse(&format!("https://{}/emojis/{}", MISSKEY_HOST, name))?;
+    let is_valid = url.scheme() == "https"
+        && url.host_str() == Some(MISSKEY_HOST)
+        && url.username().is_empty()
+        && url.password().is_none()
+        && (url.port().is_none() || url.port() == Some(443))
+        && url.query().is_none()
+        && url.fragment().is_none()
+        && MISSKEY_GET_EMOJI_PATH_PATTERN.is_match(url.path());
+    if is_valid {
+        Ok(url)
+    } else {
+        Err("Invalid Misskey emoji URL".into())
+    }
+}
+
+fn validate_download_url(raw: &str) -> Result<Url, Box<dyn Error>> {
+    let url = Url::parse(raw)?;
+    let is_valid = url.scheme() == "https"
+        && url.host_str() == Some(MISSKEY_DOWNLOAD_HOST)
+        && url.username().is_empty()
+        && url.password().is_none()
+        && (url.port().is_none() || url.port() == Some(443))
+        && url.query().is_none()
+        && url.fragment().is_none()
+        && MISSKEY_DOWNLOAD_EMOJI_PATH_PATTERN.is_match(url.path());
+    if is_valid {
+        let mut canonical_url = Url::parse(&format!("https://{}", MISSKEY_DOWNLOAD_HOST))?;
+        canonical_url.set_path(url.path());
+        Ok(canonical_url)
+    } else {
+        Err("Invalid Misskey emoji media URL".into())
     }
 }
